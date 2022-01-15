e70839fb9e668e246f0b1c1ce36dc2d8b9fa0e51
Ankita Anant <40429566+anantankita@users.noreply.github.com>
2022-01-15 18:21:59 +0530

    Add files via upload

---
+++ useless-invention.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Enter 7 if you enjoyed Local Hack Day"<<endl;
+	cin>>n;
+
+	if(n==7){
+		cout<<"Hi there! Really glad to hear that. Hope to see you in Global Hack Week :)"<<endl;
+	}
+
+	else{
+		cout<<"I think you mistakenly pressed wrong key. Try again!"<<endl;
+	}
+}
